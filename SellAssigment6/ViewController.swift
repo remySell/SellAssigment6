21846f7e9790f21b5be123ba34e9c61b2b16a67f
RemySell <r.sell@setonhill.edu>
2020-04-07 10:40:45 -0400

    Initial Commit

---
+++ SellAssigment6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SellAssigment6
+//
+//  Created by Remy Sell on 4/7/20.
+//  Copyright © 2020 Remy Sell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
